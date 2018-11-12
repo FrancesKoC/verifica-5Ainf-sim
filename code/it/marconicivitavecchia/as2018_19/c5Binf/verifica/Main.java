7bf4b8ac8469268775ed65e2259dc3a342da15d1
Your Name <you@example.com>
2018-11-12 16:55:57 +0100

    consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package compito;
 
 import javax.swing.*;
 
@@ -12,16 +12,24 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(800,600);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
+		StringBuilder sb = new StringBuilder();
 		
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<html>");
+		sb.append("<body>");
+		sb.append("<table style=width:100%>");
+		sb.append("<tr><th><h1> Formula 1 Passion </h1></th></tr><br>");
+		sb.append("<th> Pilota </th>");
+		sb.append("<th><h3>Punteggio</h3></th>);
+		sb.append("</body>");
 		
 		// TODO Convertire lo StringBuilder in String
-		
+		String html= sb.toString();
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(html));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
